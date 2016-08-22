0b3b270806c7162e39c21652d0e889296dc93b77
crazyyy <crazyyy@gmail.com>
2016-08-22 15:54:44 +0300

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,6 @@ var htmlOWp = true,
   // and set wordpress domain
   wpDomain = 'wp-framework.dev';
 
-var AUTOPREFIXER_BROWSERS = [
-  'ie >= 8', 'ie_mob >= 10', 'ff >= 20', 'chrome >= 24', 'safari >= 5', 'opera >= 12', 'ios >= 7', 'android >= 2.3', '> 1%', 'last 4 versions', 'bb >= 10'
-];
 
 if (htmlOWp === true) {
   var basePaths = {
